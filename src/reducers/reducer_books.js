2f80b3f24478e3c3f663bcb8321c8f6a6d3cfdbb
Di Wang <jackiewang5566@hotmail.com>
2017-09-09 20:57:57 -0400

    add pages detail on book-detail container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        { title: 'Javascript: The Good Parts' },
-        { title: `You Don't Know JS` },
-        { title: 'Cracking the coding interview' },
-        { title: 'Data structure and Algorithm' }
+        { title: 'Javascript: The Good Parts', pages: 400 },
+        { title: `You Don't Know JS`, pages: 500  },
+        { title: 'Cracking the coding interview', pages: 600 },
+        { title: 'Data structure and Algorithm', pages: 800  }
     ];
 }
